2e088eb4f4ffa6fe5a1911080193d3b6cc6959df
kondike_barr <kondike_barr@DevDome.DevDome.com>
2026-04-02 08:20:09 -0500

    simple backup
    gotta make sure i dont lose progress ;)

--- REP/Modules/Shop/Ocean_Shop.py
+++ REP/Modules/Shop/Ocean_Shop.py
@@ -1,5 +1,5 @@
 import os
-import gc
+import gc, sys, time
 from REP.Modules.NPCs.Azure import intro_talk
 
 def clear():
@@ -11,10 +11,10 @@ shop_items = {
     "Efficient_Rod": {"tag": "ocean", "price": 800, "desc": "Great rod. good for fishing at the beach", "id": 2},
     "Superior_Rod": {"tag": "ocean", "price": 1200, "desc": "One of the best rods to wield. I bet you will be catching great fish at the beach", "id": 3},
     "Premier_Rod": {"tag": "ocean", "price": 2000, "desc": "The best rod. His name is Rodney.", "id": 4},
-    "Basic_lure": {"tag": "lure", "price": 400, "desc": "", "id": 1}, #TODO DESC
-    "Scented_lure": {"tag": "lure", "price": 700, "desc": "", "id": 2},
-    "Superior_lure": {"tag": "lure", "price": 1000,"desc": "", "id": 3},
-    "Magic_Worm": {"tag": "lure", "price": 1500, "desc": "", "id": 4},
+    "Basic_lure": {"tag": "lure", "price": 400, "desc": "Pretty decent lure.", "id": 1}, #TODO DESC
+    "Scented_lure": {"tag": "lure", "price": 700, "desc": "The lure is scented like tide pods. thats what fish like right?", "id": 2},
+    "Superior_lure": {"tag": "lure", "price": 1000,"desc": "Much like the rod, it is superior, despite being the second best.", "id": 3},
+    "Magic_Worm": {"tag": "lure", "price": 1500, "desc": "Its a worm covered in glitter.", "id": 4},
 }    
 
 class Shop_Ocean:
@@ -46,4 +46,54 @@ class Shop_Ocean:
             intro_talk(cd, data)
 
         self.cd = cd
+        sys.stdout.write(f"\033]0;Azure's Cove\a")
+        sys.stdout.flush()
+
+        choice = 500
+        clear()
+        print(lore["ocean_shop1"]) # Despite the narrator leaving, he is still narrating from out side....somehow.
+        time.sleep(self.cd["text_timing"])
+        while True:
+            print(lore["ocean_shop2"]) # Azure is watching me intently from inside
+            time.sleep(self.cd["text_timing"])
+
+            print(lore["ocean_shop3"]) # 1. Advanced rods
+            time.sleep(self.cd["list_timing"]) 
+            print(lore["ocean_shop4"]) # 2. Lures
+            time.sleep(self.cd["list_timing"])
+            print(lore["ocean_shop5"]) # 4. chat
+            time.sleep(self.cd["list_timing"])
+            print(lore["ocean_shop6"]) # 0. Leave
+            time.sleep(self.cd["list_timing"])
+    
+            try:
+                choice: int = int(input("\n> "))
+            except ValueError:
+                clear()
+                print(lore["ocean_shop7"]) # You walk into the floor
+                time.sleep(self.cd["text_timing"])
+    
+            if choice == 0:
+                clear()
+                print(lore["ocean_shop8"]) # You leave the cove, back to the sandy beach.
+                time.sleep(self.cd["read_timer"])
+                clear()
+                gc.collect()
+                return
+
+            match choice:
+                case 1:
+                    time.sleep(self.cd["text_timing"])
+                    clear()
+                    self.main_shop("ocean", data, lore)
+                case 2:
+                    time.sleep(self.cd["text_timing"])
+                    clear()
+                    self.main_shop("lure", data, lore)
+                case _:
+                    time.sleep(self.cd["text_timing"])
+                    clear()
+                    print(lore["ocean_shop9"]) # Another floor. Please stop walking into stuff.
+                    time.sleep(self.cd["text_timing"])
+                    continue
 
